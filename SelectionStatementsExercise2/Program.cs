06da733dd7d2e669addda0eed6378531d0a70ee8
Michael Resendez <michael.resendez1@hotmail.com>
2024-10-14 14:09:17 -0500

    Exercise 2 complete with notes inside scope

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Prompt the user for their favorite school subject here
+            Console.WriteLine("What is your favorite school subject?");
+            string favoriteSubject = Console.ReadLine();
+
+            // Use a switch statement to respond to the user's input here
+            switch (favoriteSubject.ToLower()) // Convert to lower case for Case-Insensitivity
+            {
+                case "math":
+                    Console.WriteLine("Math is cool because it's all about solving problems!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science helps the world work but I still don't understand it myself!");
+                    break;
+                case "history":
+                    Console.WriteLine("History was my best class and teaches us about our past and shapes the future!");
+                    break;
+                case "english":
+                    Console.WriteLine("English is great for expressing an idea or even show creativity!");
+                    break;
+                case "art":
+                    Console.WriteLine("Art allows the brain to explore your creativity and imagination and it's a free grade in college!");
+                    break;
+                default:
+                    Console.WriteLine("Interesting choice! What do you like about it?");
+                    break;
+                    //Tried writing this scope in a different manner than the Exercise called for... 
+            }
         }
     }
 }
